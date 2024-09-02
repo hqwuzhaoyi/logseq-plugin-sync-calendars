e02b0ec19f339a48731da0f6474fc89f8026c9cc
hqwuzhaoyi <hqwuzhaoyi@gmail.com>
2024-09-03 00:10:49 +0800

    feat: Update CalDAV sync functionality

--- apps/next/app/api/route.ts
+++ apps/next/app/api/route.ts
@@ -1,36 +1,18 @@
-import { DAVCalendar, DAVCalendarObject, DAVClient } from "tsdav";
+import { LogeseqTodo, syncCalendar } from "sync-calendar";
 
-let isLogin = false;
-
-const client = new DAVClient({
-  serverUrl: "https://caldav.icloud.com",
-  credentials: {
-    username: process.env.APPLE_USER_NAME,
-    password: process.env.APPLE_USER_PASSWORD,
-  },
-  authMethod: "Basic",
-  defaultAccountType: "caldav",
-});
-
-async function loginDavClient() {
-  if (!isLogin) {
-    try {
-      await client.login();
-      isLogin = true;
-      console.log("Logged in to CalDAV server successfully.");
-      return true;
-    } catch (error) {
-      console.error("Error logging into CalDAV server:", error);
-      throw error; // Throw the error so it can be handled where login is called
-    }
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    console.log("body", body);
+    const logseqTodos: LogeseqTodo[] = body.todos;
+    await syncCalendar(logseqTodos);
+    return new Response("Successfully created events in calendar.", {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error processing POST request:", error);
+    return new Response("Error processing POST request", {
+      status: 500,
+    });
   }
-  return true;
-}
-
-export async function GET(request: Request) {
-  console.log("process.env", process.env.APPLE_USER_NAME);
-  await loginDavClient();
-  return new Response("Logged in to CalDAV server successfully.", {
-    status: 200,
-  });
 }
