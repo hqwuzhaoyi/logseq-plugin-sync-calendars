e660019dc4badf1fa9d52c3fe45fafbeefc191a5
Prajna <hqwuzhaoyi@gmail.com>
2024-10-26 23:53:40 +0800

    feat: 添加勾选需要同步的TODO功能
    支持更多标签 TODO SCHEDULED DEADLINE LATER NOW

--- apps/logseq/src/App.tsx
+++ apps/logseq/src/App.tsx
@@ -6,6 +6,8 @@ import { ofetch } from "ofetch";
 import { settingsState } from "./state/settings";
 import { useRecoilValue } from "recoil";
 // TODO: 同步TODO到日历，增加删除和选择同步功能
+// TODO: 勾选需要同步的TODO
+// 支持更多标签 TODO SCHEDULED DEADLINE LATER NOW
 
 dayjs.extend(customParseFormat);
 
