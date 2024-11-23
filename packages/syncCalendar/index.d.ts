21767be53ca30521486b64f8766516530010bd9c
hqwuzhaoyi <hqwuzhaoyi@gmail.com>
2024-11-23 23:52:01 +0800

    feat: 删除 syncCalendar 的类型定义和映射文件，更新 tsconfig 配置

--- packages/syncCalendar/index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface LogeseqTodo {
-    text: string;
-    scheduledTime: string;
-    uid: string;
-    isAllDay: boolean;
-}
-export declare function syncCalendar(logseqTodos: LogeseqTodo[]): Promise<void>;
-//# sourceMappingURL=index.d.ts.map
